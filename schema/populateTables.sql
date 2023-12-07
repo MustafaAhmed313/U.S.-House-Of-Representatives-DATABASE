eb297c234aeba2e56f5f808e3d5f9f0f421cac66
Mustafa Ahmed <mostafa3132004@gmail.com>
2023-12-07 17:37:40 +0200

    update state records

--- schema/populateTables.sql
+++ schema/populateTables.sql
@@ -1,54 +1,53 @@
 
 -- STATE RECORDS
 
-INSERT INTO state (state_name, region) VALUES 
-('Alabama', 'Southeast'),
-('Alaska', 'Northeast'),
-('Arizona', 'Southeast'),
-('Arkansas', 'Southeast'),
-('California', 'West'),
-('Colorado', 'West'),
-('Connecticut', 'Northeast'),
-('Delaware', 'Northeast'),
-('Florida', 'Southeast'),
-('Georgia', 'Southeast'),
-('Hawaii', 'West'),
-('Idaho', 'West'),
-('Illinois', 'Northeast'),
-('Indiana', 'Midwest'),
-('Iowa', 'Midwest'),
-('Kansas', 'Midwest'),
-('Kentucky', 'Southeast'),
-('Louisiana', 'Southeast'),
-('Maine', 'Northeast'),
-('Maryland', 'Northeast'),
-('Massachusetts', 'Northeast'),
-('Michigan', 'Northeast'),
-('Minnesota', 'West'),
-('Mississippi', 'Southeast'),
-('Missouri', 'Midwest'),
-('Montana', 'West'),
-('Nebraska', 'Midwest'),
-('Nevada', 'West'),
-('New Hampshire', 'Northeast'),
-('New Jersey', 'Northeast'),
-('New Mexico', 'Southwest'),
-('New York', 'Northeast'),
-('North Carolina', 'Southeast'),
-('North Dakota', 'Midwest'),
-('Ohio', 'Northeast'),
-('Oklahoma', 'Southeast'),
-('Oregon', 'West'),
-('Pennsylvania', 'Northeast'),
-('Rhode Island', 'Northeast'),
-('South Carolina', 'Southeast'),
-('South Dakota', 'Midwest'),
-('Tennessee', 'Southeast'),
-('Texas', 'Southeast'),
-('Utah', 'West'),
-('Vermont', 'Northeast'),
-('Virginia', 'Southeast'),
-('Washington', 'Northwest'),
-('West Virginia', 'Southeast'),
-('Wisconsin', 'Northeast'),
-('Wyoming', 'West');
+INSERT INTO state (state_name, region) VALUES ('Alabama', 'Southeast');
+INSERT INTO state (state_name, region) VALUES ('Alaska', 'Northeast');
+INSERT INTO state (state_name, region) VALUES ('Arizona', 'Southeast');
+INSERT INTO state (state_name, region) VALUES ('Arkansas', 'Southeast');
+INSERT INTO state (state_name, region) VALUES ('California', 'West');
+INSERT INTO state (state_name, region) VALUES ('Colorado', 'West');
+INSERT INTO state (state_name, region) VALUES ('Connecticut', 'Northeast');
+INSERT INTO state (state_name, region) VALUES ('Delaware', 'Northeast');
+INSERT INTO state (state_name, region) VALUES ('Florida', 'Southeast');
+INSERT INTO state (state_name, region) VALUES ('Georgia', 'Southeast');
+INSERT INTO state (state_name, region) VALUES ('Hawaii', 'West');
+INSERT INTO state (state_name, region) VALUES ('Idaho', 'West');
+INSERT INTO state (state_name, region) VALUES ('Illinois', 'Northeast');
+INSERT INTO state (state_name, region) VALUES ('Indiana', 'Midwest');
+INSERT INTO state (state_name, region) VALUES ('Iowa', 'Midwest');
+INSERT INTO state (state_name, region) VALUES ('Kansas', 'Midwest');
+INSERT INTO state (state_name, region) VALUES ('Kentucky', 'Southeast');
+INSERT INTO state (state_name, region) VALUES ('Louisiana', 'Southeast');
+INSERT INTO state (state_name, region) VALUES ('Maine', 'Northeast');
+INSERT INTO state (state_name, region) VALUES ('Maryland', 'Northeast');
+INSERT INTO state (state_name, region) VALUES ('Massachusetts', 'Northeast');
+INSERT INTO state (state_name, region) VALUES ('Michigan', 'Northeast');
+INSERT INTO state (state_name, region) VALUES ('Minnesota', 'West');
+INSERT INTO state (state_name, region) VALUES ('Mississippi', 'Southeast');
+INSERT INTO state (state_name, region) VALUES ('Missouri', 'Midwest');
+INSERT INTO state (state_name, region) VALUES ('Montana', 'West');
+INSERT INTO state (state_name, region) VALUES ('Nebraska', 'Midwest');
+INSERT INTO state (state_name, region) VALUES ('Nevada', 'West');
+INSERT INTO state (state_name, region) VALUES ('New Hampshire', 'Northeast');
+INSERT INTO state (state_name, region) VALUES ('New Jersey', 'Northeast');
+INSERT INTO state (state_name, region) VALUES ('New Mexico', 'Southwest');
+INSERT INTO state (state_name, region) VALUES ('New York', 'Northeast');
+INSERT INTO state (state_name, region) VALUES ('North Carolina', 'Southeast');
+INSERT INTO state (state_name, region) VALUES ('North Dakota', 'Midwest');
+INSERT INTO state (state_name, region) VALUES ('Ohio', 'Northeast');
+INSERT INTO state (state_name, region) VALUES ('Oklahoma', 'Southeast');
+INSERT INTO state (state_name, region) VALUES ('Oregon', 'West');
+INSERT INTO state (state_name, region) VALUES ('Pennsylvania', 'Northeast');
+INSERT INTO state (state_name, region) VALUES ('Rhode Island', 'Northeast');
+INSERT INTO state (state_name, region) VALUES ('South Carolina', 'Southeast');
+INSERT INTO state (state_name, region) VALUES ('South Dakota', 'Midwest');
+INSERT INTO state (state_name, region) VALUES ('Tennessee', 'Southeast');
+INSERT INTO state (state_name, region) VALUES ('Texas', 'Southeast');
+INSERT INTO state (state_name, region) VALUES ('Utah', 'West');
+INSERT INTO state (state_name, region) VALUES ('Vermont', 'Northeast');
+INSERT INTO state (state_name, region) VALUES ('Virginia', 'Southeast');
+INSERT INTO state (state_name, region) VALUES ('Washington', 'Northeast');
+INSERT INTO state (state_name, region) VALUES ('West Virginia', 'Southeast');
+INSERT INTO state (state_name, region) VALUES ('Wisconsin', 'Northeast');
+INSERT INTO state (state_name, region) VALUES ('Wyoming', 'West');
